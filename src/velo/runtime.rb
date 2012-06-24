f3ef39010189c285fff5f415ce11e1ce20c3f59b
Cat's Eye Technologies <cpressey@gmail.com>
2012-06-23 21:28:47 -0500

    Show that delegation, in some small form, works.

--- src/velo/runtime.rb
+++ src/velo/runtime.rb
@@ -5,11 +5,29 @@ require 'velo/ast'
 
 debug "loading runtime"
 
+class VeloMethod
+  def run obj, args
+    # subclass must implement something more intelligent than this plz
+    puts "arrrgghhh!"
+  end
+end
+
+class FooMethod < VeloMethod
+  def run obj, args
+    puts "foo method called on #{obj} with args #{args}!"
+  end
+end
+
 class VeloObject
-  def initialize
+  def initialize title
+    @title = title
     @parents = []
     @methods = {}
     @attrs = {}
+  end
+
+  def to_s
+    "#{@title}(#{@parents},#{@methods},#{@attrs})"
   end
 
   # let this object delegate to another object
@@ -35,9 +53,27 @@ class VeloObject
       x = nil
       for parent in @parents
         x = parent.lookup ident
-        break if not obj.nil?
+        break if not x.nil?
       end
       x
     end
   end
+
+  def call_method ident, args
+    method = lookup ident
+    method.run self, args
+  end
+end
+
+if $0 == __FILE__
+  $debug = true
+  # A toy objectbase
+
+  velo_Object = VeloObject.new 'Object'
+  velo_Object.set_method 'foo', FooMethod.new
+
+  velo_String = VeloObject.new 'String'
+  velo_String.extend velo_Object
+
+  velo_String.call_method 'foo', [1,2,3]
 end

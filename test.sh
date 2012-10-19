a8f45bd83bb826ced2c16b981cb3a4199924be91
catseye <cpressey@gmail.com>
2012-10-19 12:21:28 +0100

    Fix executable permissions.  Simplify test runner.

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-cd src && falderal test ../tests/config/Velo.markdown ../README.markdown
+cat >test_config <<EOF
+    -> Functionality "Interpret Velo Script" is implemented by shell command
+    -> "./velo.rb %(test-file)"
+EOF
+cd src && falderal test ../test_config ../README.markdown
+rm ../test_config
--- tests/config/Velo.markdown
+++
@@ -1,4 +0,0 @@
-Testing Velo scripts on Velo reference interpreter
-
-    -> Functionality "Interpret Velo Script" is implemented by shell command
-    -> "./velo.rb %(test-file)"
